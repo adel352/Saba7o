5aaca954c872e05bb3415bf06f657eb89d57742f
Adel Araji <adelaraji@adels-macbook-pro.local>
2018-06-08 22:14:06 -0400

    Initial Commit

---
+++ Saba7o/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saba7o
+//
+//  Created by Adel Araji on 2018-06-08.
+//  Copyright © 2018 Adel Araji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
